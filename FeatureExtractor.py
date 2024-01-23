6738331a4c172fc3e0dc50e85ab1f963b8dff5b1
Christopher Madison <ChrisMadison13@gmail.com>
2024-01-23 15:28:47 -0500

    added featurespaces and modelbuilder class

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -3,6 +3,8 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.feature_selection import SelectKBest, f_regression
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+from keras.utils import FeatureSpace
 
 class FeatureExtractor:
     def __init__(self, df):
@@ -12,6 +14,15 @@ class FeatureExtractor:
         self.cleanData()
         self.extract()
 
+
+        val_dataframe = self.df.sample(frac=0.2, random_state=1337)
+        train_dataframe = self.df.drop(val_dataframe.index)
+        self.val_ds = self.readyForTF(val_dataframe)
+        self.train_ds = self.readyForTF(train_dataframe)
+        self.train_ds = self.train_ds.batch(32)
+        self.val_ds = self.val_ds.batch(32)
+        self.featurespace = self.makeFeatureSpace()
+
     def extract(self):
         X = self.df
         y = self.res["y"]
@@ -19,6 +30,8 @@ class FeatureExtractor:
         X_new = selk.fit_transform(X, y)
         col = selk.get_feature_names_out()
         new_df = pd.DataFrame(columns=col, data=X_new)
+        print(new_df.dtypes)
+        new_df["y"] = y
         self.df = new_df
 
     def makeOutput(self):
@@ -38,7 +51,7 @@ class FeatureExtractor:
     
     def cleanData(self):
         types = self.df.dtypes
-        exitCols = ["WINS", "LOSSES", "PO_WINS", "PO_LOSSES", "W", "L", "W_PCT"]
+        exitCols = ["WINS", "LOSSES", "PO_WINS", "PO_LOSSES", "W", "L", "W_PCT","CONF_RANK", "DIV_RANK", "W_RANK", "L_RANK"]
         for i in types.index:
             if types[i] == "object":
                 self.df = self.df.drop(columns=i)
@@ -48,8 +61,44 @@ class FeatureExtractor:
     def printDF(self):
         print(self.df.info())
 
-    def getFeatures(self):
-        return self.df, self.res["y"]
+    def readyForTF(self, data):
+        df = data.copy()
+        labels = df.pop("y")
+        ds = tf.data.Dataset.from_tensor_slices((dict(df), labels))
+        ds = ds.shuffle(buffer_size=len(df))
+        return ds
+
+    def getDatasets(self):
+        return self.train_ds, self.val_ds
+
+    def getFeatureSpace(self):
+        return self.featurespace
+    
+    def makeFeatureSpace(self):
+        features = {
+            "WIN_PCT":"float_normalized",
+            "FG_PCT":"float_normalized",
+            "FG3_PCT":"float_normalized",
+            "DREB":"float_normalized",
+            "TOV": "float_normalized",
+            "PTS_RANK":"float_normalized",
+            "E_OFF_RATING":"float_normalized",
+            "E_DEF_RATING":         "float_normalized",
+            "E_NET_RATING":         "float_normalized",
+            "E_AST_RATIO":          "float_normalized",
+            "E_REB_PCT":            "float_normalized",
+            "E_TM_TOV_PCT":         "float_normalized",
+            "W_PCT_RANK":           "float_normalized",
+            "E_OFF_RATING_RANK":    "float_normalized",
+            "E_DEF_RATING_RANK":    "float_normalized",
+            "E_NET_RATING_RANK":    "float_normalized",
+            "E_AST_RATIO_RANK":     "float_normalized",
+            "E_DREB_PCT_RANK":      "float_normalized",
+            "E_REB_PCT_RANK":       "float_normalized",
+            "E_TM_TOV_PCT_RANK":    "float_normalized"
+        }
+        feature_space = FeatureSpace(features=features, output_mode="concat")
+        return feature_space
 
 
 
---
+++ ModelBuilder.py
@@ -0,0 +1,12 @@
+import CollectData
+import FeatureExtractor
+import tensorflow as tf
+from keras.utils import FeatureSpace
+
+class ModelBuilder:
+
+    def __init__(self) -> None:
+        pass
+
+    
+
